66f9e27bd50a148f28405752a409fa1059ad71fe
Anthony Bisulco <abisulco@gmail.com>
2017-11-28 18:37:41 -0500

    Website V2 Updates

--- js/creative.js
+++ js/creative.js
@@ -35,12 +35,6 @@
         }
     );
 
-    // Offset for Main Navigation
-    $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
 
     // Initialize WOW.js Scrolling Animations
     new WOW().init();

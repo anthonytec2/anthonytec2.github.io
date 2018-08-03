d2451f2318d864690a1d50267f0845f7ca425cca
Anthony Bisulco <bisulco.a@husky.neu.edu>
2018-08-03 15:37:39 -0400

    Attempting to fix build

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer --http-status-ignore 999 ./_site
+bundle exec htmlproofer ._site/ --url_ignore '/vendor[s]?/','/js/' --http-status-ignore "999" --allow_hash_href
